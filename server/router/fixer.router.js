8be67a977fa1113c83cb044f7e5928b7743e346a
ecoknows <jerico.villaraza19@gmail.com>
2021-12-17 00:46:38 +0800

    Fixing Fixer

--- server/router/fixer.router.js
+++ server/router/fixer.router.js
@@ -8,27 +8,28 @@ const FixerRouter = express.Router();
 FixerRouter.get(
   "/verify/account-number",
   expressAsyncHandler(async (req, res) => {
-    const users_verification = await UserVerification.find({
-      account_number: { $exists: false },
-    });
-
-    for (let i = 0; i < users_verification.length; i++) {
-      const user = await User.findById(users_verification[i].user_id);
-
-      // console.log("Users: ", users.length);
-
-      const user_verification = await UserVerification.findById(
-        users_verification[i]._id
-      );
-
-      user_verification.account_number = user.account_number;
-
-      user_verification.save();
-    }
-
-    res.send({ message: users.length });
+    VerifyAccount();
+    res.send({ message: "DONE!" });
   })
 );
 
-async function VerifyAccount() {}
+async function VerifyAccount() {
+  const users_verification = await UserVerification.find({
+    account_number: { $exists: false },
+  });
+
+  for (let i = 0; i < users_verification.length; i++) {
+    const user = await User.findById(users_verification[i].user_id);
+
+    // console.log("Users: ", users.length);
+
+    const user_verification = await UserVerification.findById(
+      users_verification[i]._id
+    );
+
+    user_verification.account_number = user.account_number;
+
+    user_verification.save();
+  }
+}
 export default FixerRouter;
