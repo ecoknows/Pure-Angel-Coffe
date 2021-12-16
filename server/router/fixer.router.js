2135b46e81f238a541017d43e4c0bc99196c7eec
ecoknows <jerico.villaraza19@gmail.com>
2021-12-17 01:10:53 +0800

    Adding

--- server/router/fixer.router.js
+++ server/router/fixer.router.js
@@ -8,7 +8,22 @@ const FixerRouter = express.Router();
 FixerRouter.get(
   "/verify/account-number",
   expressAsyncHandler(async (req, res) => {
-    VerifyAccount();
+    const users_verification = await UserVerification.find({
+      account_number: { $exists: false },
+    });
+
+    for (let i = 0; i < users_verification.length; i++) {
+      const user = await User.findById(users_verification[i].user_id);
+
+      const user_verification = await UserVerification.findById(
+        users_verification[i]._id
+      );
+
+      user_verification.account_number = user.account_number;
+
+      user_verification.save();
+    }
+
     res.send({ message: "DONE!" });
   })
 );
